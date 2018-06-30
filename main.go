448e3c2e05c6ffcb59e6d56393fbe2dfc8329597
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2018-06-30 18:46:22 +0900

    Implement basic code into CLI

--- main.go
+++ main.go
@@ -5,17 +5,34 @@ import (
 	"log"
 
 	flags "github.com/jessevdk/go-flags"
+	tcpswarm "github.com/m-mizutani/tcpswarm/lib"
 )
 
 func main() {
 	var opts struct {
-		Version bool `short:"v" long:"version" description:"Show version"`
+		PcapFile  string `short:"r" description:"Read a pcap file "`
+		Interface string `short:"i" description:"Monitor network interface"`
+		Version   bool   `short:"v" long:"version" description:"Show version"`
 	}
 
-	args, err := flags.Parse(&opts)
+	_, err := flags.Parse(&opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(args)
+	swarm, err := tcpswarm.New(tcpswarm.Config{
+		FileName:   opts.PcapFile,
+		DeviceName: opts.Interface,
+	})
+
+	if err != nil {
+		log.Fatal("initialize error:", err)
+	}
+	fmt.Println(swarm)
+	msgCh, err := swarm.Start()
+
+	for {
+		msg := <-msgCh
+		fmt.Println(msg)
+	}
 }
